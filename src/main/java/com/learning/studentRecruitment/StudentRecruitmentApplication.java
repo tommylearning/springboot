ee752e5d5cf15ca29ded00e1200db7ee730344a9
tommyliew <tommy@mayvenr.com>
2022-02-16 10:46:17 +0800

    initialize project

---
+++ src/main/java/com/learning/studentRecruitment/StudentRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.learning.studentRecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRecruitmentApplication.class, args);
+	}
+
+}
